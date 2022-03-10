31c4c47f65e976e8997f47034316a115c6443931
Bui Van Luat <91833241+vanluatbui@users.noreply.github.com>
2022-03-10 21:33:26 +0700

    Update apiServer.js

--- apiServer.js
+++ apiServer.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 //require('dotenv').load()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
